83f4c975b2a5747074b3934c4e5a4e1b2a4219b5
TheWisePigeon <zozozozeph@gmail.com>
2023-08-24 10:51:05 +0000

    add init command

--- cmd/init.go
+++ cmd/init.go
@@ -1,1 +1,13 @@
 package cmd;
+
+import (
+  "github.com/spf13/cobra"
+)
+
+var InitCmd = &cobra.Command{
+  Use:"init",
+  Short: "Initialize a new snippets database",
+  Run: func(cmd *cobra.Command, args []string) {
+    println("Init command")
+  },
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -21,6 +20,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+  rootCmd.AddCommand(InitCmd)
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -28,14 +28,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.sniptr.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
